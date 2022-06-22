4fdc7e97034ec921f4d9c542385be281a3ac239f
timmoh-king <timothykariuki43@gmail.com>
2022-06-22 11:06:45 +0300

    assignment one

--- assets/main.js
+++ assets/main.js
@@ -2,7 +2,7 @@
  * @Author: Your name
  * @Date:   2022-06-20 17:44:33
  * @Last Modified by:   Your name
- * @Last Modified time: 2022-06-22 10:40:29
+ * @Last Modified time: 2022-06-22 11:01:13
  */
  const greetings =()=>{
     const inputValue = document.querySelector('input').value
@@ -10,6 +10,6 @@
       generatedElement.innerHTML = `<p>Hello ${inputValue}, welcome to the bootcamp </p>`
       document.body.appendChild(generatedElement)
       if(inputValue === '' || inputValue == null){
-        generatedElement.innerHTML = `<p>Please enter your name</p>`
+        generatedElement.innerHTML = `<p>Please enter your name!</p>`
       }
     }
