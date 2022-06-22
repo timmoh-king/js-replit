b3335ecf45ab8e8cd09b57a9b7c5d32552018906
timmoh-king <timothykariuki43@gmail.com>
2022-06-22 10:59:28 +0300

    submit assignment

---
+++ assets/main.js
@@ -0,0 +1,15 @@
+/**
+ * @Author: Your name
+ * @Date:   2022-06-20 17:44:33
+ * @Last Modified by:   Your name
+ * @Last Modified time: 2022-06-22 10:40:29
+ */
+ const greetings =()=>{
+    const inputValue = document.querySelector('input').value
+    const generatedElement = document.createElement('p');
+      generatedElement.innerHTML = `<p>Hello ${inputValue}, welcome to the bootcamp </p>`
+      document.body.appendChild(generatedElement)
+      if(inputValue === '' || inputValue == null){
+        generatedElement.innerHTML = `<p>Please enter your name</p>`
+      }
+    }
